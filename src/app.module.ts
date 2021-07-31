8850287f159824915b1386395525d18d9e3add0a
Odilomar <odilomar.junior@gmail.com>
2021-07-31 13:26:06 -0300

    feat(docker): setup docker compose and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [    
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: String(process.env.DB_HOST),
+      port: Number(process.env.DB_PORT),
+      username: String(process.env.DB_USER),
+      password: String(process.env.DB_PASS),
+      database: String(process.env.DB_NAME),
+      entities: [__dirname + '../../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+      dropSchema: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

c5fcf1c7363f6d724e2fb1f95079b1adb8e2dbba
Odilomar <odilomar.junior@gmail.com>
2021-08-01 03:00:41 -0300

    fix(app): update build files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'dotenv';
+import path from 'path/posix';
 import { UserModule } from './user/user.module';
 
 config();
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME, SSL_CERT, SSL_KEY } = process.env;
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: String(process.env.DB_HOST),
-      port: Number(process.env.DB_PORT),
-      username: String(process.env.DB_USER),
-      password: String(process.env.DB_PASS),
-      database: String(process.env.DB_NAME),
+      host: String(DB_HOST),
+      port: Number(DB_PORT),
+      username: String(DB_USER),
+      password: String(DB_PASS),
+      database: String(DB_NAME),
       entities: [__dirname + '../**/*.entity{.ts,.js}'],
-      ssl: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       synchronize: true,
       logging: true,
       dropSchema: false,

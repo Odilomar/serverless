278c58bd17712621074c4664b279523fb87c605d
Odilomar <odilomarjunior.contact@gmail.com>
2021-08-09 17:37:21 -0300

    fix(serverless): fix serverless proxy

--- src/serverless.ts
+++ src/serverless.ts
@@ -1,48 +1,11 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
 import { NestFactory } from '@nestjs/core';
-import { Module, ValidationPipe } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Handler, Context } from 'aws-lambda';
 
 import { ExpressAdapter } from '@nestjs/platform-express';
-import awsServerlessExpress from 'aws-serverless-express';
-import { eventContext } from 'aws-serverless-express/middleware';
 import { Server } from 'http';
-import express from 'express';
-
-import { config } from 'dotenv';
-import path from 'path';
-
-const UserModule = require(path.resolve('user', 'user.module.js')) || require('./user/user.module');
-// import { UserModule } from path.resolve(__dirname, 'user', 'user.module.js');
-
-config();
-
-const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
-
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: String(DB_HOST),
-      port: Number(DB_PORT),
-      username: String(DB_USER),
-      password: String(DB_PASS),
-      database: String(DB_NAME),
-      entities: [__dirname + '../**/*.entity{.ts,.js}'],
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      synchronize: true,
-      logging: true,
-      dropSchema: false,
-      autoLoadEntities: true,
-      keepConnectionAlive: true,
-    }),
-    UserModule,
-  ],
-})
-export class AppModule { }
+import { AppModule } from './app.module';
+import * as serverless from 'aws-serverless-express';
+import { proxy } from 'aws-serverless-express';
 
 const binaryMimeTypes: string[] = [];
 let cachedServer: Server;
@@ -55,31 +18,15 @@ process.on('uncaughtException', (reason) => {
   console.error(reason);
 });
 
-const bootstrapServer = async (): Promise<Server> => {
-  try {
-    const expressApp = express();
-    const adapter = new ExpressAdapter(expressApp);
-    const app = await NestFactory.create(AppModule, adapter);
-    app.useGlobalPipes(new ValidationPipe());
-    app.enableCors();
-    app.use(eventContext());
+function bootstrapServer(): Promise<Server> {
+  const expressApp = require('express')();
+  const adapter = new ExpressAdapter(expressApp);
+  return NestFactory.create(AppModule, adapter, { logger: false })
+    .then((app) => app.init())
+    .then(() => serverless.createServer(expressApp));
+}
 
-    await app.init();
-    return awsServerlessExpress.createServer(
-      expressApp,
-      undefined,
-      binaryMimeTypes,
-    );
-  } catch (error) {
-    return Promise.reject(error);
-  }
-};
-
-export const handler: Handler = async (event, context: Context) => {
-  if (!cachedServer) {
-    const server = await bootstrapServer();
-    cachedServer = server;
-  }
-  return awsServerlessExpress.proxy(cachedServer, event, context, 'PROMISE')
-    .promise;
+export const handler: Handler = async (event: any, context: Context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
 };
